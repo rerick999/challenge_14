b2902792879e221a915e0f7e50e0fb721f3f26ec
rerick999 <127711017+rerick999@users.noreply.github.com>
2023-06-26 08:53:46 -0700

    working on charts

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -35,17 +35,48 @@ function getmetadata(sample) {
  
 function buildcharts(sample) {
 	let url="https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+	let oi=[];
+	let sv=[];
 	
-	/*
 	d3.json(url).then(data=>{
-		let idnames=data.names;
-		for (let i=0;i<idnames.length;i++){
-			selector.append("option").text(idnames[i]).property("value",idnames[i]);
+		let thedata=data.samples;
+		let filteredarray=thedata.filter(sampleobj=>sampleobj.id==sample);
+		let result=filteredarray[0];
+		console.log(result);
+		oi=result.otu_ids;
+		sv=result.sample_values;
+		
+		/*
+		console.log(result);
+		let oi=[];
+		let y=[];
+		
+		for (let i=0;i<thedata.length;i++){
+			oi.push(result.otu_ids[i]);
 		}
-		let firstsample=idnames[0];
-		optionChanged(firstsample);
+		*/
+		
+		//let firstsample=idnames[0];
+		//optionChanged(firstsample);
+		//*/
+	let sa=parallelSort([sv,oi]);
+	console.log(sa);
+	sv=sa[0];
+	oi=sa[1];
+	sv=sv.slice(0,10);
+	oi=oi.slice(0,10);
+	let oil=[];
+	for (let i=0;i<oi.length;i++){
+		oil.push("OTU "+oi[i]);
+	}
+	console.log(sv);
+	console.log(oi);
+	console.log(oil);
+	data = [{type: 'bar', orientation: 'h', x: sv, y: oil}];
+	Plotly.newPlot("bar", data);
 	});
 
+	/*
 	d3.json(url).then(data=>{
 		let metadata=data.metadata;
 		let filteredarray=metadata.filter(sampleobj=>sampleobj.id==sample);
@@ -66,8 +97,25 @@ function buildcharts(sample) {
 // Build a Horizontal Bar Chart
   }
   
-
   
+function parallelSort(arrays, comparator = (a, b) => (a > b) ? -1 : (a < b) ? 1 : 0) {
+  let arrayKeys = Object.keys(arrays);
+  let sortableArray = Object.values(arrays)[0];
+  let indexes = Object.keys(sortableArray);
+  let sortedIndexes = indexes.sort((a, b) => comparator(sortableArray[a], sortableArray[b]));
+
+  let sortByIndexes = (array, sortedIndexes) => sortedIndexes.map(sortedIndex => array[sortedIndex]);
+
+  if (Array.isArray(arrays)) {
+    return arrayKeys.map(arrayIndex => sortByIndexes(arrays[arrayIndex], sortedIndexes));
+  } else {
+    let sortedArrays = {};
+    arrayKeys.forEach((arrayKey) => {
+      sortedArrays[arrayKey] = sortByIndexes(arrays[arrayKey], sortedIndexes);
+    });
+    return sortedArrays;
+  }
+}
 
   
   // Initialize the dashboard
